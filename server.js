17211ba5fd9e1870d65e64fc90d26da44cf9c2d7
minecraftchandan <87936472+minecraftchandan@users.noreply.github.com>
2025-06-02 10:11:31 +0530

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-// server.js
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.send("Bot is alive!"));
-
-app.listen(3000, () => console.log("Web server running"));
